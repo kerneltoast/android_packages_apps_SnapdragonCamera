a8ed2ae741566365d43d34f74eefce2fc8015ec1
Sultanxda <sultanxda@gmail.com>
2015-11-20 15:23:03 -0800

    SnapdragonCamera: Disable panorama feature
    Extremely buggy.

--- src/com/android/camera/ui/ModuleSwitcher.java
+++ src/com/android/camera/ui/ModuleSwitcher.java
@@ -111,6 +111,9 @@ public class ModuleSwitcher extends RotateImageView
         // Always decrement one because of GCam.
         --numDrawIds;
 
+        // Always decrement one because of pano.
+        --numDrawIds;
+
         int[] drawids = new int[numDrawIds];
         int[] moduleids = new int[numDrawIds];
         int ix = 0;
@@ -119,6 +122,9 @@ public class ModuleSwitcher extends RotateImageView
                 continue; // not enabled, so don't add to UI
             }
             if (i == GCAM_MODULE_INDEX) {
+                continue; // don't add to UI
+            }
+            if (i == WIDE_ANGLE_PANO_MODULE_INDEX) {
                 continue; // don't add to UI
             }
             moduleids[ix] = i;
